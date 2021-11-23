1815582f30396b46c6ce98e791483c117696f739
karlakristina <karla.kristina.skegro@fpmoz.sum.ba>
2021-11-23 14:14:24 +0100

    izmjene

--- index.php
+++ index.php
@@ -1,23 +1,118 @@
+<?php
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+ $db_host= "localhost";
+ $db_user = "root";
+ $db_password = "";
+ $db_name = "pzi_projekt";
+ $mysqli= new mysqli($db_host, $db_user, $db_password, $db_name);
+ // Check conncetion
+ if ($mysqli->connect_errno) {
+	 echo "Failed to connect to MySQL:" . $mysqli -> connect_error;
+	 exit();
+ }    else {
+     echo "Uspjesno ste spojeni na bazu!";
+	 $sql = "SELECT * FROM users";
+	 
+	 $result = $mysqli ->query($sql);
+	 
+	 if ($result) {
+		 $row = $result-> fetch_row();
+		 echo json_encode($row) ;
+	 }
+	 $ime = $_GET["first_name"];
+$prezime = $_GET["last_name"];
+
+//var_dump($_GET);
+$insert_query = "INSERT INTO users (first_name, last_name) values ('$ime', '$prezime')";
+var_dump($insert_query);
+$result = $mysqli->query($insert_query);
+echo $result; 
+	 
+		 
+		 
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
+		
+		<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="first_name" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="last_name" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+  </form>
+
+
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+            
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title" title="Laragon">Laragon</div>
+     
+                <div class="info"><br />
+                      <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
+                      PHP version: <?php print phpversion(); ?>   <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
+                      Document Root: <?php print ($_SERVER['DOCUMENT_ROOT']); ?><br />
+
+                </div>
+                <div class="opt">
+                  <div><a title="Getting Started" href="https://laragon.org/docs">Getting Started</a></div>
+                </div>
+            </div>
+
+        </div>
+    </body>
 </html>
